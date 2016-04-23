5bac922769487b5f952962d9d57409c0079af5d1
Matthew Israelson <misraelson@gmail.com>
2016-04-23 16:15:00 -0700

    password resets and resets mailers

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,36 @@
+class PasswordResetsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by_email params[:email]
+    if user
+      user.generate_password_reset_data
+      PasswordResetsMailer.send_instructions(user).deliver_later
+    end
+  end
+
+  def edit
+    @user = User.find_by_password_reset_token! params[:id]
+  end
+
+  def update
+    @user = User.find_by_password_reset_token! params[:id]
+    user_params = params.require(:user).permit(:password, :password_confirmation)
+    if @user.password_reset_expired?
+      redirect_to
+      new_password_reset_path, alert:
+      "password reset expired please try again!"
+    elsif @user.update user_params
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Password reset was big success"
+    else
+      render :edit
+    end
+
+
+  end
+
+end
